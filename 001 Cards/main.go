f943f700c81872b37654a2ec00d68ffa5ca48c68
Mau Munguia <maumg1196@gmail.com>
2022-02-05 23:25:53 -0600

    Create a variable with a func

--- 001 Cards/main.go
+++ 001 Cards/main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of Spades"
+	// var card string = "Five of Diamonds"
+	// card := "Five of Diamonds" Only ':=' to init the variable
+	// card = "Ace of Spades" Only '=' in the reassignments
+	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }

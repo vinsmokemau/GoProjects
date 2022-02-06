3fc94ec16b304eeb43012d02e8bcda6da575a77a
Mau Munguia <maumg1196@gmail.com>
2022-02-05 23:41:05 -0600

    Create a slice type variable

--- 001 Cards/main.go
+++ 001 Cards/main.go
@@ -6,8 +6,13 @@ func main() {
 	// var card string = "Five of Diamonds"
 	// card := "Five of Diamonds" Only ':=' to init the variable
 	// card = "Ace of Spades" Only '=' in the reassignments
-	card := newCard()
-	fmt.Println(card)
+	// card := newCard() Create a variable with a function
+	cards := []string{"Ace of Spades", newCard()}
+	cards = append(cards, "Six of Picas") // Create a new slice and assign to the variable
+
+	for index, card := range cards {
+		fmt.Println(index, card)
+	}
 }
 
 func newCard() string {

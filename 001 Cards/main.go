3e3e5912e5630b88ee40bb78461983430395279a
Mau Munguia <maumg1196@gmail.com>
2022-02-17 00:08:06 -0600

    Add newDeck and deal functions

--- 001 Cards/main.go
+++ 001 Cards/main.go
@@ -1,20 +1,15 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Five of Diamonds"
 	// card := "Five of Diamonds" Only ':=' to init the variable
 	// card = "Ace of Spades" Only '=' in the reassignments
 	// card := newCard() Create a variable with a function
-	cards := []string{"Ace of Spades", newCard()}
-	cards = append(cards, "Six of Picas") // Create a new slice and assign to the variable
+	// cards := deck{"Ace of Spades", newCard()} Create a deck with static and function
+	// cards = append(cards, "Six of Picas") Create a new slice and assign to the variable
 
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	hand, reaminingCards := deal(cards, 5)
+	hand.print()
+	reaminingCards.print()
 }

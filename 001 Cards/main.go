54cab112a38baaed604c97001bc69487f8d998ff
Mau Munguia <maumg1196@gmail.com>
2022-03-11 15:13:57 -0600

    Add shuffle function to decks

--- 001 Cards/main.go
+++ 001 Cards/main.go
@@ -7,9 +7,12 @@ func main() {
 	// card := newCard() Create a variable with a function
 	// cards := deck{"Ace of Spades", newCard()} Create a deck with static and function
 	// cards = append(cards, "Six of Picas") Create a new slice and assign to the variable
+	// cards := newDeck() Create a Deck with our function in 'deck.go'
 
-	cards := newDeck()
-	hand, reaminingCards := deal(cards, 5)
-	hand.print()
-	reaminingCards.print()
+	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
+	cards.print()
+	// hand, reaminingCards := deal(cards, 5)
+	// hand.print()
+	// reaminingCards.print()
 }
